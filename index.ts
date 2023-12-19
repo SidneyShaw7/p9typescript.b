1eb2947c318bb9347cfb32f8871f17f5e5b80ab3
SidneyShaw7 <dmitriev.nikitos@gmail.com>
2023-12-19 08:01:50 -0500

    9.5. endpoint for get req added. 9.6. eslint implemented

--- index.ts
+++ index.ts
@@ -1,28 +1,48 @@
-import express from 'express';
-import { bmiCalculator } from './bmiCalculator';
-import { parseArguments } from './utils';
+import express from 'express'
+import { bmiCalculator } from './bmiCalculator'
+// import { calculateExercises } from './exerciseCalculator';
 
-const app = express();
-
-// interface Values {
-//   value1: number;
-//   value2: number;
-// }
+const app = express()
+app.use(express.json())
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
-});
+  res.send('Hello Full Stack!')
+})
 
 app.get('/bmi', (req, res) => {
-  //   const height = req.query.height;
-  //   const weight = req.query.weight;
-  res.send();
-  const queryParams = req.query;
-  const parsedArgs = parseArguments(queryParams);
-});
+  const height = parseFloat(req.query.height as string)
+  const weight = parseFloat(req.query.weight as string)
 
-const PORT = 3003;
+  if (height === undefined || weight === undefined) {
+    res
+      .status(400)
+      .json({ error: 'Both height and weight parameters are required' })
+    return
+  }
+
+  if (isNaN(height) || isNaN(weight)) {
+    res.status(400).json({ error: 'Invalid height or weight parameters' })
+    return
+  }
+
+  if (!isNaN(height) && !isNaN(weight)) {
+    const result = bmiCalculator(height, weight)
+    console.log(height, weight)
+    res.json({ height: height, weight: weight, bmi: result })
+  } else {
+    res.status(400).send('Invalid height or weight parameters')
+  }
+})
+
+// app.post('/calculate', (req, res) => {
+//   const { value1, value2, op } = req.body;
+
+//   const result = calculator(value1, value2, op);
+//   res.send({ result });
+// });
+
+const PORT = 3003
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
