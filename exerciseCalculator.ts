bf622c25230c9cd1b8dab083ac588477ec7b4c92
SidneyShaw7 <dmitriev.nikitos@gmail.com>
2023-12-20 15:10:54 -0500

    9.7 working on post req, testing file added

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,34 +1,34 @@
-import { parseArguments } from './utils'
+import { parseArguments } from './utils';
 
 interface ResultValues {
-  periodLength: number
-  trainingDays: number
-  success: boolean
-  rating: number
-  ratingDescription: string
-  target: number
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
 interface GivenValues {
-  value1: number
-  value2: number[]
+  value1: number;
+  value2: number[];
 }
 
 export const calculateExercises = (
   target: number,
   hoursDays: number[]
 ): ResultValues => {
-  const periodLength = hoursDays.length
-  const trainingDays = hoursDays.filter((h) => h !== 0).length
+  const periodLength = hoursDays.length;
+  const trainingDays = hoursDays.filter((h) => h !== 0).length;
   const average =
     hoursDays.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
       0
-    ) / hoursDays.length
-  let success = false
+    ) / hoursDays.length;
+  let success = false;
   if (average >= target) {
-    success = true
+    success = true;
   }
   if (periodLength === trainingDays && success) {
     return {
@@ -39,7 +39,7 @@ export const calculateExercises = (
       ratingDescription: 'You did a great job! Keep grinding!',
       target: target,
       average: average,
-    }
+    };
   } else if (periodLength / 2 <= trainingDays && success) {
     return {
       periodLength: periodLength,
@@ -49,7 +49,7 @@ export const calculateExercises = (
       ratingDescription: 'Not bad, but you can do better. Keep grinding!',
       target: target,
       average: average,
-    }
+    };
   } else {
     return {
       periodLength: periodLength,
@@ -59,23 +59,23 @@ export const calculateExercises = (
       ratingDescription: 'Stop doing shit. Start grinding!',
       target: target,
       average: average,
-    }
+    };
   }
-}
+};
 
 try {
   // console.log(calculateExercises([2, 2, 2, 0, 1, 3, 2], 1));
 } catch (error) {
-  console.error('Something went wrong:', error)
+  console.error('Something went wrong:', error);
 }
 
 try {
-  const { value1, value2 } = parseArguments<GivenValues>(process.argv)
-  console.log(calculateExercises(value1, value2))
+  const { value1, value2 } = parseArguments<GivenValues>(process.argv);
+  console.log(calculateExercises(value1, value2));
 } catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
+  let errorMessage = 'Something went wrong: ';
   if (error instanceof Error) {
-    errorMessage += error.message
+    errorMessage += error.message;
   }
-  console.log(errorMessage)
+  console.log(errorMessage);
 }
--- index.ts
+++ index.ts
@@ -1,48 +1,72 @@
-import express from 'express'
-import { bmiCalculator } from './bmiCalculator'
-// import { calculateExercises } from './exerciseCalculator';
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import express from 'express';
+import { bmiCalculator } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!')
-})
+  res.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (req, res) => {
-  const height = parseFloat(req.query.height as string)
-  const weight = parseFloat(req.query.weight as string)
+  const height = parseFloat(req.query.height as string);
+  const weight = parseFloat(req.query.weight as string);
 
   if (height === undefined || weight === undefined) {
     res
       .status(400)
-      .json({ error: 'Both height and weight parameters are required' })
-    return
+      .json({ error: 'Both height and weight parameters are required' });
+    return;
   }
 
   if (isNaN(height) || isNaN(weight)) {
-    res.status(400).json({ error: 'Invalid height or weight parameters' })
-    return
+    res.status(400).json({ error: 'Invalid height or weight parameters' });
   }
 
   if (!isNaN(height) && !isNaN(weight)) {
-    const result = bmiCalculator(height, weight)
-    console.log(height, weight)
-    res.json({ height: height, weight: weight, bmi: result })
+    const result = bmiCalculator(height, weight);
+    console.log(height, weight);
+    res.json({ height: height, weight: weight, bmi: result });
   } else {
-    res.status(400).send('Invalid height or weight parameters')
+    res.status(400).send('Invalid height or weight parameters');
   }
-})
+});
 
-// app.post('/calculate', (req, res) => {
-//   const { value1, value2, op } = req.body;
+app.post('/exercises', (req, res) => {
+  const { value1, value2 } = req.body;
+  if (
+    typeof value1 === 'number' &&
+    Array.isArray(value2) &&
+    value2.every((item) => typeof item === 'number')
+  ) {
+    console.log(value1, value2);
+    res.json(value1);
 
-//   const result = calculator(value1, value2, op);
-//   res.send({ result });
-// });
+    const result = calculateExercises(value1, value2);
+    res.send({ result });
+  } else if (!value1 || !value2) {
+    res.status(400).send('parameters missing');
+    console.log(value1, value2);
+  } else if (
+    !parseFloat(value1) ||
+    !value2.map((value: string) => parseFloat(value))
+  ) {
+    res.status(400).send('wrong parameters');
+    console.log(value1, value2);
+  }
+  // const target = parseFloat(value1);
+  // const dailyExercises = value2.map((value: any) => Number(value));
+  // const result = calculateExercises(target, dailyExercises);
+  // res.send({ result });
+});
 
-const PORT = 3003
+const PORT = 3002;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
